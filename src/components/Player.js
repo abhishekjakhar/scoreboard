3362ee50219478bf06e70c5d343193c26bc4f56a
abhishekjakhar <abhishekjakhar.11@gmail.com>
2020-04-19 22:09:03 +0530

    Icon component added

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Counter from './Counter'
+import Icon from './Icon'
 
 const Player = () => (
   <div className="player">
     <span className="player-name">
       <button className="remove-player">✖</button>
+      <Icon />
       Name
     </span>
-
     <Counter />
   </div>
 )
