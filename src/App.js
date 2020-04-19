0bd04ce0ec4ced28af6b1b14e2be9413440d72e2
abhishekjakhar <abhishekjakhar.11@gmail.com>
2020-04-19 19:22:50 +0530

    PlayForm component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import PlayerForm from './components/PlayerForm'
 import './App.css'
 
 function App() {
   return (
     <div className="scoreboard">
       <Header />
+      <PlayerForm />
     </div>
   )
 }
---
+++ src/components/PlayerForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PlayerForm = () => (
+  <form>
+    <input type="text" placeholder="Enter a player's name" />
+    <input type="submit" value="Add Player" />
+  </form>
+)
+
+export default PlayerForm
