ba3f7088189b27b623c6463af4d9742636801834
abhishekjakhar <abhishekjakhar.11@gmail.com>
2020-04-20 18:38:11 +0530

    Header test cases updated

--- src/components/__tests__/Header.js
+++ src/components/__tests__/Header.js
@@ -2,18 +2,19 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Header from '../Header'
 
-const mockPlayers = []
-
 test('the value of title in header should be Scoreboard', () => {
+  //Arrange
+  const playersData = []
+
   //rendering the component with default title prop
   //Act
-  const { rerender } = render(<Header players={mockPlayers} />)
+  const { rerender } = render(<Header players={playersData} />)
   //Assert
   expect(screen.getByTestId('title')).toHaveTextContent('Scoreboard')
 
   //re-render the same component with different title prop
   //Act
-  rerender(<Header players={mockPlayers} title="Playerboard" />)
+  rerender(<Header players={playersData} title="Playerboard" />)
   //Assert
   expect(screen.getByTestId('title')).toHaveTextContent('Playerboard')
 })
