b96ccfd0e802f368d9fd126b6b249026abc4b4fc
abhishekjakhar <abhishekjakhar.11@gmail.com>
2020-04-20 11:22:47 +0530

    Stopwatch logic added + SVG icon class names updated

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Stopwatch = () => (
-  <div className="stopwatch">
-    <h2 className="stopwatch-heading">Stopwatch</h2>
-    <span className="stopwatch-time">0</span>
-    <button>Start</button>
-    <button>Reset</button>
-  </div>
-)
+const Stopwatch = () => {
+  const [running, setRunning] = useState(false)
+  const [elapsedTime, setElapsedTime] = useState(0)
+  const [previousTime, setPreviousTime] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => tick(), 100)
+    return () => clearInterval(interval)
+  })
+
+  const tick = () => {
+    if (running) {
+      const now = Date.now()
+      setPreviousTime(now)
+      setElapsedTime(elapsedTime + (now - previousTime))
+    }
+  }
+
+  const handleStopwatch = () => {
+    setRunning(!running)
+    if (!running) {
+      setPreviousTime(Date.now())
+    }
+  }
+
+  const handleReset = () => {
+    setElapsedTime(0)
+  }
+
+  console.log(elapsedTime)
+  const seconds = Math.floor(elapsedTime / 1000)
+
+  return (
+    <div className="stopwatch">
+      <h2 className="stopwatch-heading">Stopwatch</h2>
+      <span className="stopwatch-time">{seconds}</span>
+      <button onClick={handleStopwatch}>{running ? 'Stop' : 'Start'}</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  )
+}
 
 export default Stopwatch
